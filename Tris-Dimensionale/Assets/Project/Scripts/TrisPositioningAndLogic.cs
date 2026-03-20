942af462193c6ae10732dc2ab3e7c660deba16a0
CJ2225CJ <jonatancanuti@gmail.com>
2026-03-20 13:10:37 +0100

    start programming

---
+++ Tris-Dimensionale/Assets/Project/Scripts/TrisPositioningAndLogic.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class TrisPositioningAndLogic : MonoBehaviour
+{
+    private GameObject F1P1;
+    private GameObject F1P2;
+    private GameObject F1P3;
+    private GameObject F1P4;
+    private GameObject F1P5;
+    private GameObject F1P6;
+    private GameObject F1P7;
+    private GameObject F1P8;
+    private GameObject F1P9;
+
+    private GameObject F2P1;
+    private GameObject F2P2;
+    private GameObject F2P3;
+    private GameObject F2P4;
+    private GameObject F2P5;
+    private GameObject F2P6;
+    private GameObject F2P7;
+    private GameObject F2P8;
+    private GameObject F2P9;
+
+    private GameObject F3P1;
+    private GameObject F3P2;
+    private GameObject F3P3;
+    private GameObject F3P4;
+    private GameObject F3P5;
+    private GameObject F3P6;
+    private GameObject F3P7;
+    private GameObject F3P8;
+    private GameObject F3P9;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            Debug.Log("F1-P1");
+        }
+    }
+}
